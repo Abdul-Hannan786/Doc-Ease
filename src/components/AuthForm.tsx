91c95e3257ceae17551e2b1770a5060a989d9b4e
Abdul-Hannan786 <abdulhannanhere@gmail.com>
2025-01-01 01:11:25 +0500

    Auth Layout

--- src/components/AuthForm.tsx
+++ src/components/AuthForm.tsx
@@ -26,24 +26,41 @@ import Image from "next/image";
 import Link from "next/link";
 import { PasswordInput } from "./PasswordInput";
 
-const formSchema = z.object({
-  fullName: z.string().min(2).max(50),
-});
-
 type AuthFromType = "sign-in" | "sign-up";
+
+const authFormSchema = (formType: AuthFromType) => {
+  return z.object({
+    email: z.string().email(),
+    password: z
+      .string()
+      .min(8, "Password must be at least 8 characters long")
+      .max(32, "Password must not exceed 32 characters"),
+    userType:
+      formType === "sign-up"
+        ? z
+            .string()
+            .refine(
+              (value) => ["patient", "doctor"].includes(value),
+              "User type must be either 'Patient' or 'Doctor'"
+            )
+        : z.string().optional(),
+  });
+};
 
 const AuthForm = ({ type }: { type: AuthFromType }) => {
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
   const [isLoading, setIsLoading] = useState(false);
 
+  const formSchema = authFormSchema(type);
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
     defaultValues: {
-      fullName: "",
+      email: "",
+      password: "",
+      userType: "",
     },
   });
 
-  // 2. Define a submit handler.
   const onSubmit = async (values: z.infer<typeof formSchema>) => {
     console.log(values);
   };
@@ -94,37 +111,37 @@ const AuthForm = ({ type }: { type: AuthFromType }) => {
           />
           {type === "sign-up" && (
             <FormField
-            control={form.control}
-            name="userType"
-            render={({ field }) => (
-              <FormItem>
-                <div className="shad-form-item">
-                  <FormControl>
-                    <Select onValueChange={field.onChange} defaultValue={field.value}>
-                      <SelectTrigger
-                        className="border-none shadow-none p-0 h-auto focus:ring-0 focus:outline-none focus-visible:ring-0"
+              control={form.control}
+              name="userType"
+              render={({ field }) => (
+                <FormItem>
+                  <div className="shad-form-item">
+                    <FormControl>
+                      <Select
+                        onValueChange={field.onChange}
+                        defaultValue={field.value}
                       >
-                        <SelectValue
-                          placeholder="Signup as"
-                          className="text-light-200 body-2 placeholder:text-light-200"
-                        />
-                      </SelectTrigger>
-                      <SelectContent className="border-none">
-                        <SelectItem value="patient" className="border-none">
-                          Patient
-                        </SelectItem>
-                        <SelectItem value="doctor" className="border-none">
-                          Doctor
-                        </SelectItem>
-                      </SelectContent>
-                    </Select>
-                  </FormControl>
-                </div>
-                <FormMessage className="shad-form-message" />
-              </FormItem>
-            )}
-          />
-          
+                        <SelectTrigger className="border-none shadow-none p-0 h-auto focus:ring-0 focus:outline-none focus-visible:ring-0">
+                          <SelectValue
+                            placeholder="Signup as"
+                            className="text-light-200 body-2 placeholder:text-light-200"
+                          />
+                        </SelectTrigger>
+                        <SelectContent className="border-none">
+                          <SelectItem value="patient" className="border-none">
+                            Patient
+                          </SelectItem>
+                          <SelectItem value="doctor" className="border-none">
+                            Doctor
+                          </SelectItem>
+                        </SelectContent>
+                      </Select>
+                    </FormControl>
+                  </div>
+                  <FormMessage className="shad-form-message" />
+                </FormItem>
+              )}
+            />
           )}
           <Button
             type="submit"
