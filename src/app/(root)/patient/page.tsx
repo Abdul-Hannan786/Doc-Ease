df10f7c15ebe8154b24d4bc8e2440d5abc49114a
Abdul-Hannan786 <abdulhannanhere@gmail.com>
2025-01-09 02:13:07 +0500

    Update

--- src/app/(root)/patient/page.tsx
+++ src/app/(root)/patient/page.tsx
@@ -1,3 +1,5 @@
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
 import React from "react";
 
 const PatientDashboard = () => {
@@ -55,8 +57,20 @@ const PatientDashboard = () => {
   return (
     <div className="page-container">
       <section className="w-full">
-        <h1 className="h1 capitalize">Dashboard</h1>
+        <div className="total-size-section">
+          <h1 className="h1 capitalize">Dashboard</h1>
+          <div className="sort-container">
+            <Link href="/patient/all-doctors">
+              <Button className="bg-brand hover:bg-brand-100 transition-all rounded-3xl button">See All Doctors</Button>
+            </Link>
+          </div>
+        </div>
       </section>
+      <div>
+        <h3 className="text-2xl font-semibold text-start capitalize">
+          Find the doctor you need
+        </h3>
+      </div>
       {dummyData.length > 0 ? (
         <section className="file-list">
           {dummyData.map(
@@ -73,16 +87,16 @@ const PatientDashboard = () => {
               fees,
             }) => (
               <div key={fullname}>
-                <h1 >{fullname}</h1>
-                <h1 >{age} years old</h1>
-                <h1 >{specialization}</h1>
-                <h1 >{experience}</h1>
-                <h1 >{gender}</h1>
-                <h1 >{hospital}</h1>
-                <h1 >{address}</h1>
-                <h1 >{number}</h1>
-                <h1 >{picture}</h1>
-                <h1 >{fees} PKR</h1>
+                <h1>{fullname}</h1>
+                <h1>{age} years old</h1>
+                <h1>{specialization}</h1>
+                <h1>{experience}</h1>
+                <h1>{gender}</h1>
+                <h1>{hospital}</h1>
+                <h1>{address}</h1>
+                <h1>{number}</h1>
+                <h1>{picture}</h1>
+                <h1>{fees} PKR</h1>
               </div>
             )
           )}
